ce78e5d6ef31f7c298c99300439d52621e1ebdfc
Virat <59180983+vrobot-2@users.noreply.github.com>
2026-04-02 14:25:07 -0400

    nnn

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -70,10 +70,8 @@ public class Robot extends TimedRobot
   public void robotPeriodic()
   {
     // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled  
-    // commands, running already-scheduled commands, removing finished or interrupted command
-s,
-    // and running subsystem periodic() methods.  This must be called from the robot's period
-ic
+    // commands, running already-scheduled commands, removing finished or interrupted commands,
+    // and running subsystem periodic() methods.  This must be called from the robot's periodic
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
 
